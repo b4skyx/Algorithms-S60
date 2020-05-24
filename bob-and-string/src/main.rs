18baeed52a57e484f6c81aebcc233001e3cc0d07
Abhishek Yadav <b4skyx@gmail.com>
2020-05-24 10:09:05 +0530

    new file:   bob-and-string/README.md 	modified:   bob-and-string/src/main.rs

--- bob-and-string/src/main.rs
+++ bob-and-string/src/main.rs
@@ -1,3 +1,39 @@
+use std::collections::HashMap;
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let mut n = String::new();
+    io::stdin().read_line(&mut n).expect("Failed");
+    let n = n.trim().parse::<u32>().unwrap();
+    for _ in 0..n {
+        let mut a = String::new();
+        let mut b = String::new();
+        io::stdin().read_line(&mut a).expect("Input Err");
+        io::stdin().read_line(&mut b).expect("Input Err");
+        let mut m1 = HashMap::new();
+        let mut m2 = HashMap::new();
+        let mut count = 0;
+        for ch in a.chars() {
+            let counter = m1.entry(ch).or_insert(0);
+            *counter += 1;
+        }
+        for ch in b.chars() {
+            let counter = m2.entry(ch).or_insert(0);
+            *counter += 1;
+        }
+        if m1 == m2 {
+            println!("0");
+            continue;
+        }
+        for ch in m1.keys() {
+            if m2.contains_key(ch) {
+                count += (m1[ch] - m2[ch] as i32).abs();
+            } else {
+                count += m1[ch];
+            }
+        }
+        for ch in m2.keys().filter(|x| !m1.contains_key(x)) {
+            count += m2[ch];
+        }
+        println!("{}", count);
+    }
 }
