1b900456007f0f62960953568b0b2c495d326553
Abhishek Yadav <b4skyx@gmail.com>
2020-05-26 20:42:05 +0530

    modified:   src/main.rs

--- minimize-the-max-diff-adj/src/main.rs
+++ minimize-the-max-diff-adj/src/main.rs
@@ -1,28 +1,8 @@
 use std::io;
 fn min(arr: Vec<i32>, n: u32) {
-    let mut k = n.clone();
-    let mut front = 0usize;
-    let mut last = arr.len() - 1;
-    if (last - front) < k as usize {
-        println!("K is too Large");
-    } else {
-        while k > 0 {
-            if (arr[front + 1] - arr[front]) > (arr[last] - arr[last - 1]) {
-                front += 1;
-            } else {
-                last -= 1;
-            }
-            k -= 1
-        }
-        let mut max_diff = 0;
-        for i in front..last {
-            if max_diff < arr[i + 1] - arr[i] {
-                max_diff = arr[i + 1] - arr[i];
-            }
-        }
-        println!("{}", max_diff);
-    }
+    unimplemented!();
 }
+
 fn read_input() -> Vec<i32> {
     let mut ipt = String::new();
     io::stdin().read_line(&mut ipt).expect("Failed to ReadLine");
